7b751ae27cf9748bc2b0a31759a8e2fd50f78325
wanghan76 <wanghann@hotmail.com>
2024-08-07 21:54:04 +0200

    add us hist

--- istocks/scripts/import_us_history.py
+++ istocks/scripts/import_us_history.py
@@ -6,4 +6,4 @@ def run():
     s.prepare_to_update_history_us()
     date_start = datetime.date(2023, 1, 1)
     date_end = datetime.date(2023, 12, 31)
-    s.re(start_date=date_start, end_date=date_end)
+    s.retrieve_history_us(start_date=date_start, end_date=date_end)
