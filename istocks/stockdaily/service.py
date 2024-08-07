d536a327e398d49211624d6bcc2a7d04e430f71a
wanghan76 <wanghann@hotmail.com>
2024-08-07 21:56:25 +0200

    add us hist

--- istocks/stockdaily/service.py
+++ istocks/stockdaily/service.py
@@ -15,7 +15,7 @@ def retrieve_history_hk(start_date, end_date):
     stocks = StockHkList.objects.filter(status=status_to_update_his).all()
     for stock in stocks:
         if stock.ak_code:
-            items = read_hk_hist(code=stock.ak_code, start_date=start_date, end_date=end_date)
+            items = read_hk_hist(code=stock.code, start_date=start_date, end_date=end_date)
             save_hk_daily(items=items, stock=stock)
             time.sleep(5)
 
@@ -23,7 +23,7 @@ def retrieve_history_hk(start_date, end_date):
 def retrieve_history_us(start_date, end_date):
     stocks = StockUsList.objects.filter(status=status_to_update_his).all()
     for stock in stocks:
-        items = read_us_hist(code=stock.code, start_date=start_date, end_date=end_date)
+        items = read_us_hist(code=stock.ak_code, start_date=start_date, end_date=end_date)
         save_hk_daily(items=items, stock=stock)
         time.sleep(5)
 
