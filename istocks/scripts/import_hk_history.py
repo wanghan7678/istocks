886289465475aac187316f9018ded5706f6d2614
wanghan76 <wanghann@hotmail.com>
2024-08-07 21:28:37 +0200

    add history hk

--- istocks/scripts/import_hk_history.py
+++ istocks/scripts/import_hk_history.py
@@ -3,7 +3,7 @@ import datetime
 
 
 def run():
-    s.prepare_to_update_daily_hk()
+    s.prepare_to_update_history_hk()
     date_start = datetime.datetime(2023, 1, 1)
     date_end = datetime.datetime(2023, 12, 31)
     s.retrieve_history_hk(start_date=date_start, end_date=date_end)
