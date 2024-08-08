5cf178a0956b34eff8c68da6d17c5a7d1a91ac36
wanghan76 <wanghann@hotmail.com>
2024-08-08 20:30:22 +0200

    add macd

--- istocks/scripts/import_us_history.py
+++ istocks/scripts/import_us_history.py
@@ -3,7 +3,7 @@ import datetime
 
 
 def run():
-    # s.prepare_to_update_history_us()
-    date_start = datetime.date(2021, 1, 1)
-    date_end = datetime.date(2021, 12, 31)
+    s.prepare_to_update_history_us()
+    date_start = datetime.date(2020, 1, 1)
+    date_end = datetime.date(2020, 12, 31)
     s.retrieve_history_us(start_date=date_start, end_date=date_end)
