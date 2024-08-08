131bcced4d769bd56ad4aabd71987961942e1429
wanghan76 <wanghann@hotmail.com>
2024-08-08 12:07:04 +0200

    add qfq functions

--- istocks/scripts/import_hk_history.py
+++ istocks/scripts/import_hk_history.py
@@ -3,7 +3,7 @@ import datetime
 
 
 def run():
-    # s.prepare_to_update_history_hk()
+    s.prepare_to_update_history_hk()
     date_start = datetime.date(2022, 1, 1)
     date_end = datetime.date(2022, 12, 31)
     s.retrieve_history_hk(start_date=date_start, end_date=date_end)
