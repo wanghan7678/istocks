c749076d1a395ef8cc1f704d4a0edf4fb5383b25
wanghan76 <wanghann@hotmail.com>
2024-08-11 20:39:18 +0200

    some refactor

--- istocks/stockdaily/signal_service.py
+++ istocks/stockdaily/signal_service.py
@@ -13,7 +13,17 @@ def cal_save_macd_hk():
     cal_update_macd_hk(start_date=start_date)
 
 
-def cal_update_macd_hk(start_date):
+def create_macd_goldens(stock_type, start_date):
+    if stock_type == s.stock_hk:
+        s.update_status_to(stock_type=s.stock_hk, status=s.status_to_update_signal)
+        cal_update_macd_hk(start_date=start_date)
+    if stock_type == s.stock_us:
+        s.update_status_to(stock_type=s.stock_us, status=s.status_to_update_signal)
+        cal_update_macd_hk(start_date=start_date)
+
+
+
+def cal_update_macd_hk(start_date, to_create):
     now_date = datetime.datetime.now().date()
     s.update_status_to(s.stock_hk, s.status_to_update_signal)
     stocks = StockHkList.objects.filter(status=s.status_to_update_signal).all()
@@ -21,7 +31,24 @@ def cal_update_macd_hk(start_date):
         dates, prices = p.get_hk_daily_closes_qfq(code=stock.code, start_date=start_date, end_date=now_date)
         items = ca.get_macd(code=stock.code, dates=dates, closes=prices)
         print("  calculate " + stock.code + " MACD Golden: totally " + len(items) + " saved.")
-        s.update_items(model_name=s.model_hk_signal, items=items, stock=stock)
+        if to_create:
+            s.insert_items(model_name=s.model_hk_signal, items=items, stock=stock)
+        else:
+            s.update_items(model_name=s.model_hk_signal, items=items, stock=stock)
+
+
+def cal_update_macd_us(start_date, to_create):
+    now_date = datetime.datetime.now().date()
+    s.update_status_to(s.stock_us, s.status_to_update_signal)
+    stocks = StockUsList.objects.filter(status=s.status_to_update_signal).all()
+    for stock in stocks:
+        dates, prices = p.get_hk_daily_closes_qfq(code=stock.code, start_date=start_date, end_date=now_date)
+        items = ca.get_macd(code=stock.code, dates=dates, closes=prices)
+        print("  calculate " + stock.code + " MACD Golden: totally " + len(items) + " saved.")
+        if to_create:
+            s.insert_items(model_name=s.model_us_signal, items=items, stock=stock)
+        else:
+            s.update_items(model_name=s.model_us_signal, items=items, stock=stock)
 
 
 def cal_signal_labels(signal):
