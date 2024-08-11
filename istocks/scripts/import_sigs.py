56d2c8c1a6655523e98c1b3cfe99d3fcfef478ef
wanghan76 <wanghann@hotmail.com>
2024-08-11 21:42:03 +0200

    some refactor

--- istocks/scripts/import_sigs.py
+++ istocks/scripts/import_sigs.py
@@ -5,7 +5,7 @@ import datetime
 def run(*args):
     if len(args) > 1:
         stock_type = args[0]
-        start_date = datetime.strptime(args[1], "%Y-%m-%d").date()
+        start_date = datetime.datetime.strptime(args[1], "%Y-%m-%d").date()
         ss.create_macd_goldens(stock_type=stock_type, start_date=start_date)
     else:
         print("input --script-args <stock_type: hk or us> <start_date: yyyy-mm-dd>")
