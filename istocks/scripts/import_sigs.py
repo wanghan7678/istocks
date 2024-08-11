4f8a412537e0af481f727153303cfffb80f71d62
wanghan76 <wanghann@hotmail.com>
2024-08-11 21:41:01 +0200

    some refactor

--- istocks/scripts/import_sigs.py
+++ istocks/scripts/import_sigs.py
@@ -1,11 +1,11 @@
 import stockdaily.signal_service as ss
+import datetime
 
 
 def run(*args):
     if len(args) > 1:
-        s.update_status_to(stock_type=s.stock_us, status=s.status_to_update_his)
         stock_type = args[0]
-        start_date = datetime.datetime.strptime(args[1], "%Y-%m-%d").date()
+        start_date = datetime.strptime(args[1], "%Y-%m-%d").date()
         ss.create_macd_goldens(stock_type=stock_type, start_date=start_date)
     else:
         print("input --script-args <stock_type: hk or us> <start_date: yyyy-mm-dd>")
