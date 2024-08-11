1d9497cf48bfabc5f7fd00b2dc65f046adc7b2ed
wanghan76 <wanghann@hotmail.com>
2024-08-11 11:08:13 +0200

    some refactor

--- istocks/scripts/import_hk_history.py
+++ istocks/scripts/import_hk_history.py
@@ -7,7 +7,7 @@ from django.db import IntegrityError
 
 def run(*args):
     if len(args) > 1:
-        s.update_status_to(stock_type=s.stock_hk, status=s.status_to_update_hist)
+        s.update_status_to(stock_type=s.stock_hk, status=s.status_to_update_his)
         date_start = datetime.datetime.strptime(args[0], "%Y-%m-%d").date()
         date_end = datetime.datetime.strptime(args[1], "%Y-%m-%d").date()
         s.read_save_history_hk(start_date=date_start, end_date=date_end)
